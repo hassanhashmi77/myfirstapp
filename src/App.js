bde505a27bf1bc4062a4971093487151e7324e37
hassanhashmi77 <66280195+hassanhashmi77@users.noreply.github.com>
2020-06-06 00:56:24 +0500

    new1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My Latest First App..</h1>
+      <h1>My Latest First App..
+        <img src="https://avatars0.githubusercontent.com/u/9892522?v=4&s=400"></img>
+      </h1>
     </div>
   );
 }
